5c7a291f3433ed2dec60a6f59c19888b897567f6
ishan086 <ishan086@gmail.com>
2018-05-21 20:42:21 -0400

    first commit

--- src/main/java/com/monoresty/mongoresty/PasswordController.java
+++ src/main/java/com/monoresty/mongoresty/PasswordController.java
@@ -1,17 +1,24 @@
 package com.monoresty.mongoresty;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class PasswordController {
-@RequestMapping(value="/password",method=RequestMethod.GET)	
 
+public class PasswordController {
+	
+	@Autowired
+	private CustomerRepository repository;
+	
+@RequestMapping(value="/password",method=RequestMethod.GET)	
 public String checkpwd(@RequestParam(value="password") String pwd)
 {
 	
+	
+	repository.save(new Customer(pwd, "come"));
  return ("hellow"+pwd);
 }
 
